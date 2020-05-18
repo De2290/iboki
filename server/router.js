9927159742e1f645bfffc09bc5279b303e6f8bc9
Bob Miller <mrbob6645@gmail.com>
2020-05-18 15:59:16 -0400

    changed create function in database.js

--- server/router.js
+++ server/router.js
@@ -1,32 +1,37 @@
-const express = require('express');
-const database = require('./database.js');
+const express = require("express");
+const database = require("./database.js");
 
 const router = express.Router();
 
 router
-.get("/", (req, res) => {
-    res.render('index');
-})
-.get("/login", (req, res) => {
-    res.render('login');
-})
-.get("/register", (req, res) => {
-    res.render('register');
-})
+  .get("/", (req, res) => {
+    res.render("index");
+  })
+  .get("/login", (req, res) => {
+    res.render("login");
+  })
+  .get("/register", (req, res) => {
+    res.render("register");
+  })
+  .get("/profile", (req, res) => {
+    res.render("profile");
+  });
 
 router
-.post("/login", (req, res) => {
-    console.log(`Username: ${req.body.username}, Password: ${req.body.password}`);
+  .post("/login", (req, res) => {
+    console.log(
+      `Username: ${req.body.username}, Password: ${req.body.password}`
+    );
     database.login(req.body.username, req.body.password);
     res.redirect("/login");
-})
-.post("/register", async (req, res) => {
+  })
+  .post("/register", async (req, res) => {
     var err = await database.create(req.body.username, req.body.password);
     console.log(err);
     if (err == 0) {
-        res.redirect("/register");
+      res.redirect("/register");
     } else {
-        res.redirect("/login");        
+      res.redirect("/login");
     }
-})
+  });
 module.exports = router;
